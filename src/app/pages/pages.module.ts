fd371d47005f6b2d8d1b8827b51a4a826d755553
rudycancax@gmail.com <rudycancax@gmail.com>
2021-02-10 08:18:47 -0600

    Modificación de Home a Dashboard

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
-import { HomeComponent } from './home/home.component';
+// Componentes
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 @NgModule({
   declarations: [
     PagesComponent,
-    HomeComponent,
+    DashboardComponent
+
   ],
   exports: [
-    HomeComponent,
+    DashboardComponent,
     PagesComponent,
   ],
   imports: [
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,15 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { PagesComponent } from './pages.component';
-import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
     { 
-        path: 'home', 
-        component: PagesComponent,
+        path: 'home', component: PagesComponent,
         children: [
-          { path: '', component: HomeComponent},
+          { path: '', component: DashboardComponent},
 
 
           
