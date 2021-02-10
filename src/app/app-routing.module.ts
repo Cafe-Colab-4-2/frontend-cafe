db64df9a9f55ae228f5332fc4bcf6af38792b87e
rudycancax@gmail.com <rudycancax@gmail.com>
2021-02-09 22:26:21 -0600

    Se agregaron las rutas hijas (Una forma, existen otras)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { LoginComponent } from './auth/login/login.component';
+import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { 
+    path: '', 
+    component: PagesComponent,
+    children: [
+      { path: 'home', component: HomeComponent},
+      { path: '', redirectTo: '/home', pathMatch: 'full' }
+    ]
+  },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent }
 ];
 
