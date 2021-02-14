bb1c4270e2ceaa4e8e34260bfb3a4745bd228100
rudycancax@gmail.com <rudycancax@gmail.com>
2021-02-13 19:43:14 -0600

    Breadcrumb finalizado

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivationEnd, Router } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -8,27 +9,31 @@ import { filter, map } from 'rxjs/operators';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
 
   titulo: string;
+  argumentosRuta: Subscription;
 
   constructor( private router: Router ) { 
-    this.getArgumentosRuta();
-  }
-
-  getArgumentosRuta() {
-    this.router.events
-    .pipe(
-      filter( event => event instanceof ActivationEnd),
-      filter( (event: ActivationEnd) => event.snapshot.firstChild === null ),
-      map(  (event: ActivationEnd) => event.snapshot.data ),
-    )
+    this.argumentosRuta = this.getArgumentosRuta()
     .subscribe( event => {
       console.log(event, event.titulo);
       this.titulo = event.titulo;
       document.title =  `Café - ${event.titulo}`
       
-    })
+    });
+  }
+  ngOnDestroy(): void {
+    this.argumentosRuta.unsubscribe();
+  }
+
+  getArgumentosRuta() {
+    return this.router.events
+    .pipe(
+      filter( event => event instanceof ActivationEnd),
+      filter( (event: ActivationEnd) => event.snapshot.firstChild === null ),
+      map(  (event: ActivationEnd) => event.snapshot.data ),
+    )
 
     /* También se puede desusctructurar el event ej 
     .suscribe( { titulo }) => {
